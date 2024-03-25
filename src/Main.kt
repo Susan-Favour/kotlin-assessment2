e9b3d7e75b288e576e5bb48650f212def0e79ee3
SusanFavour <susanfavourodongo@gmail.com>
2024-03-25 08:26:38 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+     exclusion()
+
+
+
+
+}
+
+fun exclusion (){
+    var sentence = (" Barnie bakes brown bagels and buns")
+    println(sentence.removePrefix("b"))
+}
+
+fun values (intArray: (x: Int, y: Int, z: Int)): IntArray{
+   var sum =  intArrayOf()
+
+
+}
+
+fun volume(y: Double, x: Float, z: Int){
+
+
+}
+fun isPalindrome(word: String): Boolean{
+    var checkWord = ("word")
+
+
